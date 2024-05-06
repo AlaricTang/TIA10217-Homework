c0d6b7a699bfe4eb0e92cb0ed89b0bba6078e7c0
Alaric <148807797+AlaricTang@users.noreply.github.com>
2024-05-07 01:26:38 +0800

    Update HomeworkExt_1.java

--- src/hwExt_1/HomeworkExt_1.java
+++ src/hwExt_1/HomeworkExt_1.java
@@ -34,6 +34,9 @@ class Pencil extends Pen{
 	public double getPrice() {
 		return super.getPrice()*0.8;
 	}
+	public double getPrice() {
+		return super.getPrice()*0.8;
+	}
 	public void write() {
 		System.out.println("削鉛筆再寫");
 	}
@@ -47,6 +50,9 @@ class InkBrush extends Pen{
 	public InkBrush(String brand,double price) {
 		super(brand,price);
 	}
+	public String getBrand() {
+		return super.getBrand();
+	}
 	public double getPrice() {
 		return super.getPrice()*0.9;
 	}
@@ -59,12 +65,12 @@ public class HomeworkExt_1 {
 	public static void main(String[] args) {
 		
 		Pen p[]=new Pen[2];
-		p[0] = new Pencil();
-		p[1]= new InkBrush();
+		p[0] = new Pencil("嘻嘻",1000);
+		p[1]= new InkBrush("哈哈",1000);
 		
 		for(int i=0;i<p.length;i++) {
 			p[i].write();
-			System.out.println("價格為: "+p[i].getPrice());
+			System.out.println("品牌為: "+p[i].getBrand()+"\n價格為: "+p[i].getPrice());
 		}
 	}
 }
